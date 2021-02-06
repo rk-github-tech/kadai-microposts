254e1cf3ad8b1dc66a11de1c30f26352ae84f8dd
rk <culture.ir.123@gmail.com>
2021-02-06 07:28:30 +0000

    user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    
+  end
+
+  def destroy
+    def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+  end
+  
+  private
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      # ログイン成功
+      session[:user_id] = @user.id
+      return true
+    else
+      # ログイン失敗
+      return false
+    end
+  end
+end
