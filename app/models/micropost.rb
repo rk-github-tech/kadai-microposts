ad3a093608aaa87231c0c77a89b7551aaec81b98
rk <culture.ir.123@gmail.com>
2021-02-10 14:06:02 +0000

    favorite3

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :reverses_of_favorites, class_name: 'Favorite'
+  has_many :reverses_of_favorites, class_name: 'Favorite', dependent: :destroy
   has_many :whose_favorite, through: :favorites, source: :user
 end
